b9af99c4a9415d97148469d1c235d1680fcf6a29
Ramatoulaye Bah <141573185+RamatoulayeBah28@users.noreply.github.com>
2024-02-17 15:10:57 -0600

    Updated Screen Window

--- SquirrelsTail.py
+++ SquirrelsTail.py
@@ -1,36 +1,73 @@
-import pygame as pg
+import pygame, sys
+from SquirrelsButtons import Button
 
+pygame.init()
 
-class SquirrelGame:
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
 
-    def __init__(self):
-        # initialize game window, etc
-        pg.init()
-        pg.mixer.init()
-        self.screen = pg.display.set_mode((480, 600))
-        pg.display.set_caption("A Squirrel's Tail")
-        self.clock = pg.time.Clock()
-        self.running = True
-        self.font_name = pg.font.match_font("Arial")
+BG = pygame.image.load("/Users/ramatoulaye/Downloads/SquirrelBackground.png")
 
-    def new(self):
-        # start a new game
-        self.score = 0
-        self.all_sprites = pg.sprite.Group()
-        self.platforms = pg.sprite.Group()
-        # self.player = Player(self)
-        # self.all_sprites.add(self.player)
-        # for plat in PLATFORM_LIST:
-        #     p = Platform(*plat)
-        #     self.all_sprites.add(p)
-        #     self.platforms.add(p)
-        self.run()
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.SysFont("Pixel/Bitmap", size)
 
-g = SquirrelGame()
-# g.show_start_screen()
-# while g.running:
-#     g.new()
-#     g.show_go_screen()
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-# pg.quit()
-g.running()
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+    
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("PlayGameButton.JPG"), pos=(640, 250), 
+                            text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("QuitGameButton.JPG"), pos=(640, 550), 
+                            text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+main_menu()
