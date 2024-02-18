b09e52eee4e4ef97b9ccf8c39bc393c4b29b0230
yeshedj <157068258+yeshedj@users.noreply.github.com>
2024-02-17 22:29:27 -0600

    typewriter thingie

--- SquirrelsTail.py
+++ SquirrelsTail.py
@@ -28,33 +28,51 @@ def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.SysFont("papyrus", size)
 
 def play():
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+    PLAY_BACK = Button(image=None, pos=(50, 50),
+                       text_input="BACK", font=get_font(36), base_color="White", hovering_color="Green")
 
-        SCREEN.fill("black")
+    SCREEN.fill("black")
+    message = [
+        "The love of your squirrel life is at the top! Climb up & collect hearts on the way to deliver to your beloved! Have fun!"]
+    font = get_font(36)
+    text_y = 300
+    speed = 2
+    counter = 0
+    done = False
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen LALALALALALALA.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(700, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        PLAY_BACK = Button(image=None, pos=(700, 460),
-                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-
+    while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    main_menu()
+                if PLAY_BACK.checkForInput(pygame.mouse.get_pos()):
+                    return
 
-        # SCREEN.blit(cursor_image,PLAY_MOUSE_POS)
-        pygame.display.update()
+        SCREEN.fill("black")
+        pygame.draw.rect(SCREEN, "black", [0, 300, 800, 200])
 
-    
+        if counter < speed * len(message[-1]):
+            counter += 1
+        else:
+            done = True
+
+        text_to_render = "".join([line[:counter // speed] for line in message])
+        text_surface = font.render(text_to_render, True, "white")
+        SCREEN.blit(text_surface, (10, text_y))
+
+        PLAY_BACK.changeColor(pygame.mouse.get_pos())
+        PLAY_BACK.update(SCREEN)
+
+        pygame.display.flip()
+
+        pygame.time.Clock().tick(60)
+
+    main_menu()
+
+
+
+
 def main_menu():
     while True:
         SCREEN.blit(BG, (0, 0))
@@ -64,9 +82,9 @@ def main_menu():
         MENU_TEXT = get_font(80).render("A SQUIRREL'S TAIL", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(700, 180))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("PlayGameButton.JPG"), pos=(200, 400),
+        PLAY_BUTTON = Button(image=pygame.image.load("PlayGameButton.png"), pos=(200, 400),
                             text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("QuitGameButton.JPG"), pos=(1200, 400),
+        QUIT_BUTTON = Button(image=pygame.image.load("QuitGameButton.png"), pos=(1200, 400),
                             text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
@@ -89,4 +107,9 @@ def main_menu():
         pygame.display.update()
 
 main_menu()
+
+
+
+
+
 
